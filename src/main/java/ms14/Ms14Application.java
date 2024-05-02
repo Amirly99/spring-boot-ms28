f0c3fb0863106d2b73d00d3f0d9933d526fdff32
user <ragibisayev@gamil.com>
2024-05-02 12:10:16 +0400

    new project

---
+++ src/main/java/ms14/Ms14Application.java
@@ -0,0 +1,13 @@
+package ms14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ms14Application.class, args);
+	}
+
+}
---
+++ src/test/java/ms14/Ms14ApplicationTests.java
@@ -0,0 +1,13 @@
+package ms14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
